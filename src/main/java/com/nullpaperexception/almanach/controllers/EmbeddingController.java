d3fb7b5ba3ef760dd462bcebaa34db8937784841
Silver Quettier <silver.quettier@gmail.com>
2017-06-11 19:55:03 +0200

    Commit for webhook testing

--- src/main/java/com/nullpaperexception/almanach/controllers/EmbeddingController.java
+++ src/main/java/com/nullpaperexception/almanach/controllers/EmbeddingController.java
@@ -11,7 +11,7 @@ public class EmbeddingController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Graou \\o/";
     }
 
     public static void main(String[] args) throws Exception {
